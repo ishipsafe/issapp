7b9600b92a906196edffa644ea92c4e9b377fa6f
Abhinav Belide <Abhinav Belide>
2015-11-09 20:40:47 -0800

    checking line endings

--- src/app/server/bin/automigrate.js
+++ src/app/server/bin/automigrate.js
@@ -20,19 +20,18 @@ dataSource.automigrate('Subscriber', function(err) {
   if (err) console.log(err);
 
   var Subscribe = app.models.Subscribe;
+
   //var count = accounts.length;
   /*accounts.forEach(function(account) {
     Email.create(account, function(err, record) {
       if (err) return console.log(err);
-
       console.log('Record created:', record);
-
       count--;
-
       if (count === 0) {
         console.log('done');
         dataSource.disconnect();
       }
     });
   });*/
+  
 });
